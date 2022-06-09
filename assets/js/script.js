f1cee55c121da9d12edefb2ace97343ddb1f2b3a
Asad Khilji <khiljiasad2@gmail.com>
2022-06-09 16:38:45 -0400

    added more time slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,11 @@ $(document).ready(function () {
     })  
 })  
 
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
+$("#6 .description").val(localStorage.getItem("6"));
+$("#7 .description").val(localStorage.getItem("7"));
+$("#8 .description").val(localStorage.getItem("8"));
 $("#9 .description").val(localStorage.getItem("9"));
 $("#10 .description").val(localStorage.getItem("10"));
 $("#11 .description").val(localStorage.getItem("11"));
@@ -19,6 +24,16 @@ $("#14 .description").val(localStorage.getItem("14"));
 $("#15 .description").val(localStorage.getItem("15"));
 $("#16 .description").val(localStorage.getItem("16"));
 $("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+$("#19 .description").val(localStorage.getItem("19"));
+$("#20 .description").val(localStorage.getItem("20"));
+$("#21 .description").val(localStorage.getItem("21"));
+$("#22 .description").val(localStorage.getItem("22"));
+$("#23 .description").val(localStorage.getItem("23"));
+$("#24 .description").val(localStorage.getItem("24"));
+$("#25 .description").val(localStorage.getItem("25"));
+$("#26 .description").val(localStorage.getItem("26"));
+$("#27 .description").val(localStorage.getItem("27"));
 
 function background () {
     var currentHour = moment().hours();
